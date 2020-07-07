7fc349acffc3d0dee306efe4c47f5944c9cea1f3
a.abramov <a.abramov@i-npz.ru>
2020-07-07 12:40:08 +0300

    Календарь

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  class MyApp extends Component {
+    state = {
+      date: new Date(),
+    }
+  
+    onChange = date => this.setState({ date })
+  
+    render() {
+      return (
+        <div>
+          <Calendar
+            onChange={this.onChange}
+            value={this.state.date}
+          />
+        </div>
+      );
+    }
+}
 }
 
 export default App;
